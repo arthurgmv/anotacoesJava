552a31438757fe737f771358ee338ab4a884aa07
Arthur Gabriel <arthurgabrielmv@gmail.com>
2024-05-18 16:17:04 -0300

    adicionado aula de vetores

--- src/tiposDados.java
+++ src/tiposDados.java
@@ -1,20 +1,33 @@
 public class tiposDados {
     public static void main(String[] args) {
         //tipos primários/primitivos
-        int num1 = 4; //inteiro
+        int num0 = 4; //inteiro
+        long num1 = 99999999999999l; // Inteiro muito grande
         short num2 = 3; //inteiro (número curto)
-        byte num5 = 4; //inteiro mas utiliza menos memória
-        float num3 = 23.4f; //por padrão os números com ponto flutuante são considerados double
-        double num4 = 23.4;
+        byte num3 = 4; //inteiro mas utiliza menos memória
+        float num4 = 23.4f; //por padrão os números com ponto flutuante são considerados double
+        double num5 = 23.4;
+        char num6 = 2; // um caractere
 
+        //alfanuméricos:
+        char letra = 'a'; // um caractere, SEMPRE COM ASPAS SIMPLES
+        //NÃO EXISTE O TIPO PRIMITIVO string
 
+        //tipos booleanos:
+            boolean verdadeiro = true;
+            boolean falso = false;
 
         //tipos não primarios/primitivos
         Integer numA = 98; //uma classe que representa o int
-        Short numB = 7; //uma classe que representa o short
-        Byte numC = 4; //uma classe que representa o byte
-        Float numD = 23.4f; //uma classe que representa o float
-        Double numE = 23.4; //uma classe que representa o double
+        Long numB = 999999999999L;
+        Short numC = 7; //uma classe que representa o short
+        Byte numD = 4; //uma classe que representa o byte
+        Float numE = 23.4f; //uma classe que representa o float
+        Double numF = 23.4; //uma classe que representa o double
+        Character name0 = 'a';
+        String name1 = "nome"; //SEMPRE SERÁ NÃO PRIMITIVO
+        Boolean vdd = true;
+        Boolean fls = false;
 
-    }
+    };
 }
---
+++ src/vetores.java
@@ -0,0 +1,15 @@
+public class vetores {
+    public static void main(String[] args) {
+        //vetores = arrays
+
+        //declarando um vetor
+        int vertozinho[];
+
+        //declarando e especificando o tamanho do vetor
+        int numeros[] = new int [5];
+
+        //declarando e inicializando
+        int vertorzao[] = {1,3,5,7,9};
+
+    }
+}
