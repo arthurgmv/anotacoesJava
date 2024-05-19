1c3299cf9608e875bb97e24b73148e52fef48120
Arthur Gabriel <arthurgabrielmv@gmail.com>
2024-05-18 23:37:58 -0300

    vetores e matrizes

---
+++ src/matrizes.java
@@ -0,0 +1,16 @@
+public class matrizes {
+    public static void main(String[] args) {
+        /* Matriz:
+            [linhas][colunas]
+            ex: numeros[0][0];
+                numeros[2][3];
+         */
+        //declaração e definição de tamanho da matriz
+        int numeros[][] = new int[3][3];
+
+        //declarar, definir e inicializar
+        int outros_numeros[][] = {{1,2,3},{1,2,3},{1,2,3}};
+        
+    }
+
+}
--- src/vetores.java
+++ src/vetores.java
@@ -11,5 +11,26 @@ public class vetores {
         //declarando e inicializando
         int vertorzao[] = {1,3,5,7,9};
 
-    }
+        //outros exemplos:
+        float valores[] = new float[5];
+        char caracteres[];
+        String nomes[];
+
+        //Algumas práticas com arrays:
+        int numeros2[] = new int[10];
+        System.out.println("O tamanho do verto é: " + numeros2.length);
+        for(int i = 0; i < numeros2.length; i++) {
+            numeros2[i] = i + 3;
+        }
+        System.out.println(numeros2[0]);
+        System.out.println(numeros2[9]);
+
+        int numeros3[] = new int[10];
+
+        for(int i = 0; i < numeros3.length; i++) {
+            numeros3[i] = (int)Math.round(Math.random() * 10);
+        }
+        System.out.println(numeros[0]);
+        System.out.println(6);
+}
 }
