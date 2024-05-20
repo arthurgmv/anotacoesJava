445fa71d768542b5ef4b3fd25a4c05b38e044412
Arthur Gabriel <arthurgabrielmv@gmail.com>
2024-05-19 22:28:29 -0300

    adicionado aula de funções

---
+++ src/funcoes.java
@@ -0,0 +1,43 @@
+/* faça um programa que receba diversos nomes de
+frutas do usuário e no final imprima essas frutas em ordem contrária.
+O programa deve solicitar ao usuáiro quantas frutas ele quer informar
+*/
+
+import java.util.Scanner;
+
+
+public class funcoes {
+   static String frutas[];
+   static Scanner teclado = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int qtd;
+
+        System.out.println("Informe a quantidade de frutas para cadastrar: ");
+        qtd = Integer.parseInt(teclado.nextLine());
+        cadastrar_dados(qtd);
+        mostrar_dados(qtd);
+
+        teclado.close();
+    }
+    /* uma função deve ter:
+    a) tipo de retorno (tipo de dado que a função vai retornar)
+    b) nome (corresponde a ação que a mesma realiza)
+    c) parâmetros/argumentos de entrada (opcional)
+    d) retorno (opicional - depende do tipo de retorno)
+     */
+
+    static void cadastrar_dados(int quantidade){
+        frutas = new String[quantidade];
+
+        for(int i = 0; i < quantidade; i++) {
+            System.out.println("Informe a " + (i+1) + " fruta: ");
+            frutas[i] = teclado.nextLine();
+        }
+    }
+    static void mostrar_dados(int quantidade){
+        for (int i = quantidade - 1; i >= 0; i--) {
+            System.out.println(frutas[i]);
+        }
+    }
+}
