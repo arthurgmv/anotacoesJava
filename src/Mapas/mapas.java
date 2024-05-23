7640221d5c6209fd9b2e84364247db4d294372c5
Arthur Gabriel <arthurgabrielmv@gmail.com>
2024-05-23 00:49:24 -0300

    aula de mapas

---
+++ src/Mapas/mapas.java
@@ -0,0 +1,15 @@
+package Mapas;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class mapas {
+    public static void main(String[] args) {
+        Map<Integer, String> pessoas = new HashMap<Integer, String>();
+
+        pessoas.put(1, "Pedro");
+        pessoas.put(2, "Thiago");
+        pessoas.put(3, "João");
+
+    }
+}
